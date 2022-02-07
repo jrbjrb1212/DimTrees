3b05ca248cb772dbedfc58d627c1a9c60f40e8e4
jrbjrb1212 <77088661+jrbjrb1212@users.noreply.github.com>
2022-02-06 20:08:27 -0500

    Create partialMTTKRPNEW.m

---
+++ partialMTTKRPNEW.m
@@ -0,0 +1,24 @@
+
+function T = partialMTTKRPNEW(Z,U,S,side)
+% Inputs: Z is a tensor, U is a cell array, S is the split node of the
+% tree, side is variable that describes the side of the tree(1 is left, 2
+% is right)
+
+% Output: partialMTTKRP Tensor T
+
+% could also use N = size(U,2);
+N = ndims(Z);
+
+% left partial KRP from N to S
+if side == 1
+   T = tensor(tenmat(Z,1:S-1) * khatrirao(U{N:-1:S}));
+   
+% right partial KRP from S-1 to 1
+elseif side == 2
+   T = tensor(tenmat(Z,S:N) * khatrirao(U{S-1:-1:1}));
+   
+end
+  
+
+end
+
