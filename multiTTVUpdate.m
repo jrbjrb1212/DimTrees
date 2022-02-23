bc0eb44be8d9f3a627dec6bddef8155beeee538c
jrbjrb1212 <77088661+jrbjrb1212@users.noreply.github.com>
2022-02-22 22:40:40 -0500

    Update multiTTVUpdate.m

--- multiTTVUpdate.m
+++ multiTTVUpdate.m
@@ -1,16 +1,12 @@
-function X = multiTTVUpdate(T,U)
-% Inputs: a partialMTTKRP tesnor T, a shortened amount of facotr matrices
-% U
+function [X,T_block] = multiTTVUpdate(T,U)
+% Inputs: a partialMTTKRP tensor T, a single factor matrix U
 
 % Outputs: X the updated tensor
 
 dims = size(T);
 N = length(dims);
 R = dims(N);
-dims
-prod(dims(1:N-1))
-
-Tmat = reshape(T, [prod(dims(1:dims-1)), R]);
+Tmat = reshape(double(T), [prod(dims(1:end-1)), R]);
 Xmat = zeros(prod(dims(2:N-1)), R);
 
 
@@ -18,14 +14,13 @@ for r = 1:R
     % Reshape as tensor then call ttv in all modes but the first or
     % form kronker product and then multiply on the right
     % get away from all the cases
-    T_block = reshape(Tmat(:,R),[dims(1), prod(dims(2:(N-1)))]);
+    
+    T_block = reshape(Tmat(:,r),[dims(1), prod(dims(2:(N-1)))]);
     X_block = U(:,r)' * T_block;
+    
     Xmat(:,r) = X_block(:);
 end
 
-X = reshape(Xmat, dims(2:N));
-
-% Old working way
-%X = UpdateTensor(T,U,1);
+X = tensor(reshape(Xmat, dims(2:N)));
 
 end
