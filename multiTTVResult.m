6e1feb881fd8e9e1ff8dadeaa0674e82cde82969
jrbjrb1212 <77088661+jrbjrb1212@users.noreply.github.com>
2022-03-05 00:14:48 -0500

    Update multiTTVResult.m

--- multiTTVResult.m
+++ multiTTVResult.m
@@ -9,38 +9,63 @@ N = size(U,2);
 % determines size of output MTTKRP product
 R = size(U{1},2);
 
-
 V = zeros(size(T,1),R);
-
-% could do kroncker of vectors
-%
+dims = size(T);
+Tmat = reshape(double(T), [prod(dims(1:end-1)), R]);
 
 
 for r = 1:R
-    % Set up cell array with appropriate vectors for ttv multiplication
-%     Z = cell(N-1,1);
-%     j = 1;
-%     
-    % maybe replace with cellfun
-    %Z = cellfun(@(x) x(v(x),:),C)
-   
-    Z = cellfun(@(x) x(:,r), {U{2:N}}, 'UniformOutput', false);
-    
-%     for i = 2:N
-%         list(j) = i;
-%         Z{j} = U{i}(:,r);
-%         j = j + 1;
-%     end
-%     Z{1}
-    
 
-    % Perform ttv multiplication
+    % forms vectors
+    Z = cellfun(@(x) x(:,r), U(2:N), 'UniformOutput', false);
+    
+    T_block = reshape(Tmat(:,r),[dims(1), prod(dims(2:N))]);
+    Kvector = khatrirao(Z(end:-1:1));
+    
+    %tempStore = T_block * Kvector;
+    V(:,r) = T_block * Kvector;
+end
+
+end
+
+% old info
+ % Perform ttv multiplication
     % try to do it without tensortoolbox
     % try khatrioaro
     % 1 R for loop
-    % pull out a block from 
-    % form khatraio of vectors
-    tempStore = double(ttv(T, Z, 2:N));
-    V(:,r) = tempStore(:,r);  
-end
-end
+    % pull out a block from T
+    
+    %temp_store = reshape(T, prod(dims(1:end-2)), dims(end-1));
+    %temp_store
+%
+
+
+% % old working version
+% for r = 1:R
+%     % Set up cell array with appropriate vectors for ttv multiplication
+% %     Z = cell(N-1,1);
+% %     j = 1;
+% %     
+%     % maybe replace with cellfun
+%     %Z = cellfun(@(x) x(v(x),:),C)
+%    
+%     Z = cellfun(@(x) x(:,r), {U{2:N}}, 'UniformOutput', false);
+%     
+% %     for i = 2:N
+% %         list(j) = i;
+% %         Z{j} = U{i}(:,r);
+% %         j = j + 1;
+% %     end
+% %     Z{1}
+%     
+% 
+%     % Perform ttv multiplication
+%     % try to do it without tensortoolbox
+%     % try khatrioaro
+%     % 1 R for loop
+%     % pull out a block from 
+%     % form khatraio of vectors
+%     tempStore = double(ttv(T, Z, 2:N));
+%     V(:,r) = tempStore(:,r);  
+% end
+%
