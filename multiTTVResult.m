a2fbdda5195521df9a8b8eb79ca8c6910528356d
jrbjrb1212 <77088661+jrbjrb1212@users.noreply.github.com>
2022-03-28 15:22:32 -0400

    Update multiTTVResult.m

--- multiTTVResult.m
+++ multiTTVResult.m
@@ -1,69 +1,31 @@
 function V = multiTTVResult(T,U)
-% Inputs: a partialMTTKRP tesnor T, a shortened amount of facotr matrices
-% U
+%multiTTVResult computes an mttkrp result
+%   
+%   V = multiTTVResult(T,U) computes a mttkrp result from tesnor T and a
+%   shortened amount of factor matrices. T will be multipied by the
+%   khatriao product of the factor matrix, which will yield V a singular
+%   2-way tensor. V will be the same demensions as the factor matrices 
+%   in U.
+%
 
-% Outputs: V, the MTTKRP result
+%% MTTKRP result calculation
 
+% Tensor Dimensions and Rank Calculation
 N = size(U,2);
-    
-% determines size of output MTTKRP product
 R = size(U{1},2);
-
-V = zeros(size(T,1),R);
 dims = size(T);
+
+% Matricization of T and preallocation of V
 Tmat = reshape(double(T), [prod(dims(1:end-1)), R]);
+V = zeros(size(T,1),R);
 
-
+% iterate over the rank of T
 for r = 1:R
     Z = cellfun(@(x) x(:,r), U(2:N), 'UniformOutput', false);
     Kvector = khatrirao(Z(end:-1:1));
     
     T_block = reshape(Tmat(:,r),[dims(1), prod(dims(2:N))]);
-    
-    %tempStore = T_block * Kvector;
     V(:,r) = T_block * Kvector;
 end
 
 end
-
-% old info
- % Perform ttv multiplication
-    % try to do it without tensortoolbox
-    % try khatrioaro
-    % 1 R for loop
-    % pull out a block from T
-    
-    %temp_store = reshape(T, prod(dims(1:end-2)), dims(end-1));
-    %temp_store
-%
-
-
-% % old working version
-% for r = 1:R
-%     % Set up cell array with appropriate vectors for ttv multiplication
-% %     Z = cell(N-1,1);
-% %     j = 1;
-% %     
-%     % maybe replace with cellfun
-%     %Z = cellfun(@(x) x(v(x),:),C)
-%    
-%     Z = cellfun(@(x) x(:,r), {U{2:N}}, 'UniformOutput', false);
-%     
-% %     for i = 2:N
-% %         list(j) = i;
-% %         Z{j} = U{i}(:,r);
-% %         j = j + 1;
-% %     end
-% %     Z{1}
-%     
-% 
-%     % Perform ttv multiplication
-%     % try to do it without tensortoolbox
-%     % try khatrioaro
-%     % 1 R for loop
-%     % pull out a block from 
-%     % form khatraio of vectors
-%     tempStore = double(ttv(T, Z, 2:N));
-%     V(:,r) = tempStore(:,r);  
-% end
-%
