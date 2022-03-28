f189d14373dc052253f3424d0974a6b89a110af1
jrbjrb1212 <77088661+jrbjrb1212@users.noreply.github.com>
2022-03-28 15:23:54 -0400

    Update multiTTVUpdate.m

--- multiTTVUpdate.m
+++ multiTTVUpdate.m
@@ -1,26 +1,31 @@
 function X = multiTTVUpdate(T,U)
-% Inputs: a partialMTTKRP tensor T, a single factor matrix U
+%multiTTVUpdate updates a tensor with a singular factor matrix
+%
+%   X = multiTTVUpdate(T,U) computes an update to tensor T by multiplying
+%   the tensor by a singular factor matrix U. This computation should
+%   reduce the deminsion of T by exactly one. This reduced form of T will
+%   be the return tensor X
+%
 
-% Outputs: X the updated tensor
+%% multiTTV update computation
 
+% Tensor Dimensions and Rank Calculation
 dims = size(T);
 N = length(dims);
 R = dims(N);
+
+% Matricization of T and X
 Tmat = reshape(double(T), [prod(dims(1:end-1)), R]);
 Xmat = zeros(prod(dims(2:N-1)), R);
 
-
+% iterate over the rank of T
 for r = 1:R
-    % Reshape as tensor then call ttv in all modes but the first or
-    % form kronker product and then multiply on the right
-    % get away from all the cases
-    
     T_block = reshape(Tmat(:,r),[dims(1), prod(dims(2:(N-1)))]);
     X_block = U(:,r)' * T_block;
-    
     Xmat(:,r) = X_block(:);
 end
 
+% form matricized Xmat into tensor X
 X = tensor(reshape(Xmat, dims(2:N)));
 
 end
