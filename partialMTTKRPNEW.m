118b269d6ef0fcc3dc897ed1a5c69b2db74d78e3
jrbjrb1212 <77088661+jrbjrb1212@users.noreply.github.com>
2022-03-28 15:26:08 -0400

    Update partialMTTKRPNEW.m

--- partialMTTKRPNEW.m
+++ partialMTTKRPNEW.m
@@ -1,48 +1,40 @@
 
-function T = partialMTTKRPNEW(T,U,S,side)
-% Inputs: T is a tensor, U is a cell array, S is the split node of the
-% tree, side is variable that describes the side of the tree(1 is left, 2
-% is right)
+function Z = partialMTTKRPNEW(T,U,S,side)
+%partialMTTKRPNEW computes a partial tensor of a full tensor 
+%
+%   Z = partialMTTKRPNEW(T,U,S,side) calculates a partial tensor of a full
+%   tennsor by computing all khatriaoproducts on one side of the split node
+%   and mixing them into the tensor modes on the opposite side of the split
+%   node. T is a full tensor, U is a cell array, S is the split node of 
+%   the tree, side is variable that describes the side of the root node 
+%   (1 is left, 2 is right)
+%
+%   Output: partialMTTKRP Tensor Z. Z could be the left or right tensor
+%   depending on the passed input of side
+%
 
-% Output: partialMTTKRP Tensor T
+%% Partial MTTKRP update computation
 
-% could also use N = size(U,2);
+% Tensor Dimensions and Rank Calculation
 dims = size(T);
 N = length(dims); 
 R = size(U{1}, 2);
-% dims = size(T);
-% N = ndims(T);
 
-% add tensor to the end of computation
 
-% left partial KRP from N to S
+% LEFT PARTIAL TENSOR COMPUTATION
+% Calculates the khatriao product from N to S and computes with mode
+% deminsons from 1 to S-1
 if side == 1
-     %tic
-     T = tensor(reshape(double(T),[prod(dims(1:S-1)), prod(dims(S:N))]) * khatrirao(U{N:-1:S}));
-    %toc
-    
-     T = tensor(reshape(T, [dims(1:S-1), R]));
-     
-%      if (mod(N,2) == 0)
-%          T = reshape(T, dims(N:-1:S-1));
-%      else
-%          T = reshape(T, dims(N:-1:S));
-%      end
+     Z = tensor(reshape(double(T),[prod(dims(1:S-1)), prod(dims(S:N))]) * khatrirao(U{N:-1:S}));
+     Z = tensor(reshape(Z, [dims(1:S-1), R]));
 
-% right partial KRP from S-1 to 1
-elseif side == 2
-    %tic
-    T = tensor(reshape(double(T),[prod(dims(1:S-1)), prod(dims(S:N))])' * khatrirao(U{S-1:-1:1}));
-    %tock
-
-    T = tensor(reshape(T, [dims(S:N), R]));
+% RIGHT PARTIAL TENSOR COMPUTATION
+% Calculates the khatriao product from S-1 to 1 and computes with mode
+% deminsons from N to S
+if side == 2
+    Z = tensor(reshape(double(T),[prod(dims(1:S-1)), prod(dims(S:N))])' * khatrirao(U{S-1:-1:1}));
+    Z = tensor(reshape(Z, [dims(S:N), R]));
     
-%     if (mod(N,2) == 0)
-%        T = reshape(T, dims(S:-1:1));
-%     else
-%        T = reshape(T, dims(S+1:-1:1));
-%     end
-     
-end
 end
 
+end
