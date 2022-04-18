9029da3b639292b8c89ed69d4f4bde90c660e9f1
jrbjrb1212 <77088661+jrbjrb1212@users.noreply.github.com>
2022-04-18 01:12:17 -0400

    Update MTTKRP_TEST.m

--- Test Files/MTTKRP_TEST.m
+++ Test Files/MTTKRP_TEST.m
@@ -1,15 +1,5 @@
 function [W,Y] = MTTKRP_TEST()
-% test file
-
-% do 1000x1000x1000
-% small rank 100
-
-% do 300x300x300x300
-% small rank 30
-
-% do one six way for great speed up
-% 25-30 tensor
-% smaller rank of 5
+% early file
 
 % test case 1
 % Z = tensor(rand(2,2,2,2,2,2,2,2,2,2,2,2));
@@ -22,14 +12,14 @@ function [W,Y] = MTTKRP_TEST()
 % N = 5;
 
 % test case 3
-%Z = tensor(rand(25,25,25,25,25,25));
-%X = {rand(25,25),rand(25,25),rand(25,25),rand(25,25),rand(25,25), rand(25,25)};
+% Z = tensor(rand(25,25,25,25,25,25));
+% X = {rand(25,25),rand(25,25),rand(25,25),rand(25,25),rand(25,25), rand(25,25)};
 %N = 6;
 
 % % test case 4
-% Z = tensor(rand(3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3));
-% X = {rand(3,3),rand(3,3),rand(3,3),rand(3,3),rand(3,3),rand(3,3),rand(3,3),rand(3,3),rand(3,3),rand(3,3),rand(3,3),rand(3,3),rand(3,3),rand(3,3),rand(3,3),rand(3,3),rand(3,3)};
-% N = 17;
+% Z = tensor(rand(3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3));
+% X = {rand(3,5),rand(3,5),rand(3,5),rand(3,5),rand(3,5),rand(3,5),rand(3,5),rand(3,5),rand(3,5),rand(3,5),rand(3,5),rand(3,5),rand(3,5),rand(3,5),rand(3,5),rand(3,5),rand(3,5),rand(3,5)};
+% N = 18;
 
 %test case 5
 % Z = tensor(rand(3,3,3,3,3));
@@ -42,9 +32,9 @@ function [W,Y] = MTTKRP_TEST()
 % N = 6;
 
 % test case 7
-Z = tensor(rand(150,150,150,150));
-X = {rand(150,50),rand(150,50),rand(150,50),rand(150,50)};
-N = 4;
+% Z = tensor(rand(150,150,150,150));
+% X = {rand(150,50),rand(150,50),rand(150,50),rand(150,50)};
+% N = 4;
 
 % test case 8
 % Z = tensor(rand(30,30,30,30,30));
@@ -55,15 +45,15 @@ totalRatio = 0;
 numTimes = 1;
 
 for k = 1:numTimes
-%     tic
-%     Y = cell(N,1);
-%     for n = 1:N
-%         Y{n} = mttkrp(Z,X,n);
-%     end
-%     timeOrig = toc;
+    tic
+    Y = cell(N,1);
+    for n = 1:N
+        Y{n} = mttkrp(Z,X,n);
+    end
+    timeOrig = toc
 
     % correct strucutre
-%     tic
+    tic
     
     % mode dimension of tensors
     dims = size(Z);
@@ -87,13 +77,13 @@ for k = 1:numTimes
             % LEFT KRP TENSOR calculation
             T = partialMTTKRPNEW(Z, X, S, 1);
             % Multittv for MTTKRP result
-            W{n} = multiTTVResult(T,X(1:S-1));
+            W{n} = multiTTVResult(T,X(n+1:S-1));
             
         elseif n < S-1
             % Multittv for Internal node update
             T = multiTTVUpdate(T,X{n-1});
             % Multittv for MTTKRP result
-            W{n} = multiTTVResult(T,X(n:S-1));
+            W{n} = multiTTVResult(T,X(n+1:S-1));
           
         elseif n == S-1
             %  Multittv for Internal node update
@@ -105,13 +95,13 @@ for k = 1:numTimes
             % RIGHT KRP TENSOR calculation
             T = partialMTTKRPNEW(Z, X, S, 2);
             % multittv for MTTKRP result
-            W{n} = multiTTVResult(T,X(n:N));
+            W{n} = multiTTVResult(T,X(n+1:N));
             
         elseif n < N
            % Multittv for Internal node update
            T = multiTTVUpdate(T, X{n-1});
            % Multittv for MTTKRP result
-           W{n} = multiTTVResult(T, X(n:N));
+           W{n} = multiTTVResult(T, X(n+1:N));
            
         else
            %  Multittv for Internal node update
@@ -122,8 +112,8 @@ for k = 1:numTimes
         %MATRIX{n} = T;
         
     end
-%     timeNew = toc;
-%     disp("Optimization versus Standard Ratio: " + sprintf("%0.4f",timeOrig/timeNew) + "x speed up");
+     timeNew = toc
+     disp("Optimization versus Standard Ratio: " + sprintf("%0.4f",timeOrig/timeNew) + "x speed up");
 %     totalRatio = totalRatio + (timeOrig/timeNew);
     %disp("New Time: " + timeNew + " is " + (timeOrig - timeNew) + " faster than Original Time: " + timeOrig);
 end
