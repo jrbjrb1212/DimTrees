3062dff84bb7e8237efc28f7c4ff6874176e6926
jrbjrb1212 <77088661+jrbjrb1212@users.noreply.github.com>
2022-04-18 01:11:47 -0400

    Update cp_als_test.m

--- Test Files/cp_als_test.m
+++ Test Files/cp_als_test.m
@@ -1,12 +1,12 @@
-% it is working generally
 % I use an impossible to reach tolerance to test how much fast the
 % optimization is than the default implementation
 
-T = tensor(@rand, [15,15,15,15,15,15]);
+T = tensor(@rand, [3,3,3,3,3,3]);
 R = 50;
 profile on
-Y = cp_als(T,R,'tol',1.0e-10,'maxiters',30);
-W = cp_als_new(T,R,'tol',1.0e-10,'maxiters',30);
+maxiters = 30;
+Y = cp_als_new(T,R,'tol',1.0e-10,'maxiters',maxiters,'dt', true);
+W = cp_als(T,R,'tol',1.0e-10,'maxiters',maxiters);
 
 profile viewer
 profile off
